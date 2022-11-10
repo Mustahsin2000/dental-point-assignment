9c170b37c50727dc14ef40b56cfe57e7910a8925
mustahsin2000 <mustahsin152415@gmail.com>
2022-11-10 14:12:14 +0600

    register

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,6 +5,7 @@ import logo from '../../../assets/b373087eba3a65525faed865e72707ed.jpg'
 const Header = () => {
     const menu = <>
     <li className='font-semibold'><Link to='/'>Home</Link></li>
+    <li className='font-semibold'><Link to='/login'>Login</Link></li>
     </>
     return (
         <div className="navbar bg-base-100 mb-6 mt-3">
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Services from "../../Pages/Home/Services/Services";
 import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
         {
           path:'/login',
           element:<Login></Login>
+        },
+        {
+          path:'/signup',
+          element:<SignUp></SignUp>
         }
       ]
     }
